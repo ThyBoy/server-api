475472c096b4a46a629e4bf37cfbf779dacc83f1
ThyBoy <kushwahanarendra2000@gmail.com>
2020-05-31 16:01:49 +0530

    changed

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.post("/imageapi",(req,res)=>{
     )
 })
 
-app.listen(3000, ()=> {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
